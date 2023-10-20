9b650b85ec4b8e0133c6b33b5d79b9e6713fba91
Ritupriya <ritupriyabgc@gmail.com>
2023-10-20 15:01:47 +0600

    Banner added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 import Container from "@/Components/Container/Container";
+import Banner from "./Home/Banner/Banner";
+
+
 
 
 export default function Home() {
   return (
     <div>
       <Container>
-          
+        
+          {/* Banner */}
+         <Banner></Banner>
 
     
       </Container>
