bf6f4214d1c2ab74579a5e4143ddd8b66fe16fcb
Abdur Rahman <abdurrahman27485@gmail.com>
2023-10-20 12:33:13 +0600

    login ui created

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,35 @@
+'use client'
+import AuthContainer from '@/Components/AuthContainer/AuthContainer';
+import Head from 'next/head';
+import Image from 'next/image';
 import React from 'react';
 
-const Page = () => {
+const Login = () => {
+    const inputFieldCommonCSS = 'rounded-lg p-2 text-base lg:text-lg bg-gray-200 outline-none border-2 border-gray-200  focus:border-sky-400';
+    const loginBtnCSS = 'w-full p-2 bg-gray-200 font-bold text-xl rounded-lg hover:shadow-md hover:shadow-gray-200'
     return (
-        <div>
-            <h1>hello this is Login page</h1>
-        </div>
+        <AuthContainer>
+            <div>
+                <Head>
+                    <title>Login</title>
+                </Head>
+                <Image className='mx-auto' src={'https://img.freepik.com/premium-vector/study-house-logo-template-design_316488-433.jpg'} height={150} width={250} alt='logo'></Image>
+                <form className='flex justify-center flex-col gap-6 w-full' onSubmit={(e) => e.preventDefault()}>
+                    <h1 className='text-2xl text-center font-bold'>Email Address</h1>
+                    <input className={`${inputFieldCommonCSS}`} type="email" placeholder='example@gmail.com' />
+                    {/* <div>
+                        <div className='flex justify-between'>
+                            <label className='text-xl font-semibold'>Password</label>
+                            <button className='text-xl text-sky-400 font-medium hover:underline'>Forgot Password</button>
+                        </div>
+                        <input className={`${inputFieldCommonCSS} w-full`} type="password" placeholder='XXXXXXXX' />
+                    </div> */}
+                    <button className={`${loginBtnCSS}`}>Next</button>
+                    {/* <button className={`${loginBtnCSS}`}>Login</button> */}
+                </form>
+            </div>
+        </AuthContainer>
     );
 };
 
-export default Page;
+export default Login;
