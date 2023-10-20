eb632a961b1333027194d910baf21d0967d18522
mostafizur rahman <dev.mostafizurrahman@gmail.com>
2023-10-20 15:16:45 +0600

    added footer part

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Container from "@/Components/Container/Container";
+import Footer from "@/Components/Footer/Footer";
 
 
 export default function Home() {
   return (
     <div>
       <Container>
+        <Footer></Footer>
           
 
     
