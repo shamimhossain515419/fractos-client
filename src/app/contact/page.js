78b63c531d5dbe4d1d61227100edf342a750277d
Shamimusman515419 <shamimhossain01617@gmail.com>
2023-10-20 18:08:14 +0600

    conofig

---
+++ src/app/contact/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactSection from '../../Components/Contact/index'
+
+const Contact = () => {
+     return (
+          <div>
+               <ContactSection></ContactSection>
+          </div>
+     );
+};
+
+export default Contact;
--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,39 @@
 import Container from "@/Components/Container/Container";
-<<<<<<< HEAD
+
 import Footer from "@/Components/Footer/Footer";
 import Pricing from "@/Components/Pricing/Pricing";
-=======
-<<<<<<< HEAD
+
 import Banner from "./Home/Banner/Banner";
 
 
-=======
 import Premium from "@/Components/Premium/Premium";
 import CounterSection from "@/Components/CounterSection/CounterSection";
->>>>>>> fe0de61ad91806ed0b545bc8d2b317f2c6c5927e
->>>>>>> fd54d44c2bd7e1d58f65e014f395124af9e50c46
+import ContactSection from "@/Components/Contact";
+
 
 
 export default function Home() {
   return (
     <div>
       <Container>
-<<<<<<< HEAD
-        <Pricing></Pricing>
-        <Footer></Footer>
-          
-=======
-<<<<<<< HEAD
-        
-          {/* Banner */}
-         <Banner></Banner>
->>>>>>> fd54d44c2bd7e1d58f65e014f395124af9e50c46
+        <Banner></Banner>
 
-=======
-        <Premium></Premium>
+
+        <div className=" my-3">
           <CounterSection> </CounterSection>
->>>>>>> fe0de61ad91806ed0b545bc8d2b317f2c6c5927e
-    
+        </div>
+        <div className=" py-5">
+          <Pricing></Pricing>
+        </div>
+        <div className=" py-5">
+          <Premium></Premium>
+        </div>
+
+        {/* Banner */}
+
+        <ContactSection></ContactSection>
+
+        <Footer></Footer>
       </Container>
 
     </div>
