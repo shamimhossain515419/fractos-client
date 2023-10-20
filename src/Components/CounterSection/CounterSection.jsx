0144ac408098b32b5d00d2fcfd8b9f77fb16f192
Shamiul haque <shamiulhaque1218@gmail.com>
2023-10-20 15:01:24 +0600

    Counter Section

---
+++ src/Components/CounterSection/CounterSection.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Counter from '.';
+
+
+const CounterSection = () => {
+    return (
+        <container className="grid lg:grid-cols-4 lg:py-10 py-5 grid-cols-1 gap-1 lg:gap-0 text-center lg:text-start" >
+
+         <div className='border p-8 lg:rounded-s-md rounded-md lg:rounded-none primaryBg2 text-3xl text-lime-200 font-bold'>
+            <Counter end={110000} />
+            <h1 className='text-base text-white lg:pl-4 pt-2 font-semibold'>Students</h1>
+         </div>
+
+         <div className='border p-8 primaryBg2 text-3xl text-teal-200 font-bold lg:rounded-none rounded-md'>
+            <Counter end={121000} />
+            <h1 className='text-base text-white lg:pl-4 pt-2 font-semibold'>Questions</h1>
+         </div>
+
+         <div className='border p-8 primaryBg2 text-3xl text-rose-300 font-bold lg:rounded-none rounded-md'>
+            <Counter end={20000000} />
+            <h1 className='text-base text-white lg:pl-8 pt-2 font-semibold'>Exams Taken</h1>
+         </div>
+
+         <div className='border p-8 lg:rounded-e-md rounded-md primaryBg2 text-3xl text-violet-200 font-bold lg:rounded-none'>
+            <Counter end={60000000} />
+            <h1 className='text-base text-white lg:pl-4 pt-2 font-semibold'>Minutes Practiced</h1>
+         </div>
+        </container>
+    );
+};
+
+export default CounterSection;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import Container from "@/Components/Container/Container";
+import CounterSection from "@/Components/CounterSection/CounterSection";
 
 
 export default function Home() {
   return (
     <div>
       <Container>
-          
-
+          <CounterSection> </CounterSection>
     
       </Container>
 
