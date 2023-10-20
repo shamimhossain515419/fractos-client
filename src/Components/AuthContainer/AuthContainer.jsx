c267206c64ede88d09fa7005cb3cc961a14716fe
Abdur Rahman <abdurrahman27485@gmail.com>
2023-10-20 11:23:44 +0600

    AuthContainer for login and registraion page

---
+++ src/Components/AuthContainer/AuthContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthContainer = () => {
+    return (
+        <div className='w-full max-w-lg mx-8'>
+            
+        </div>
+    );
+};
+
+export default AuthContainer;
