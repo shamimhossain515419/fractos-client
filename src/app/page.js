d81c6c1e2a8db05e005f3782964fa658b8f52033
Shamimusman515419 <shamimhossain01617@gmail.com>
2023-10-20 17:13:51 +0600

    banner-1

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,21 @@
 import Container from "@/Components/Container/Container";
-<<<<<<< HEAD
 import Banner from "./Home/Banner/Banner";
 
 
-=======
 import Premium from "@/Components/Premium/Premium";
 import CounterSection from "@/Components/CounterSection/CounterSection";
->>>>>>> fe0de61ad91806ed0b545bc8d2b317f2c6c5927e
 
 
 export default function Home() {
   return (
     <div>
       <Container>
-<<<<<<< HEAD
         
           {/* Banner */}
-         <Banner></Banner>
+           <Banner></Banner>
 
-=======
-        <Premium></Premium>
+          <Premium></Premium>
           <CounterSection> </CounterSection>
->>>>>>> fe0de61ad91806ed0b545bc8d2b317f2c6c5927e
     
       </Container>
 
