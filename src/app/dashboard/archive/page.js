d245a7d783b3a01fbcf89e3be5f6907907e210c9
Shamimusman515419 <shamimhossain01617@gmail.com>
2023-10-24 22:57:36 +0600

    archiv

--- src/app/dashboard/archive/page.js
+++ src/app/dashboard/archive/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Archive = () => {
      return (
           <div>
-               
+               <Archive></Archive>
           </div>
      );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -7,14 +7,9 @@ import Banner from "./Home/Banner/Banner";
 
 
 import Premium from "@/Components/Premium/Premium";
-// import Premium from "@/Components/Archive/Archive";
 import CounterSection from "@/Components/CounterSection/CounterSection";
 import ContactSection from "@/Components/Contact";
-<<<<<<< HEAD
 import Navbar from "@/Components/Navbar";
-=======
-import Archive from "@/Components/Archive/Archive";
->>>>>>> 740c6a143837c8fde29e5461cda43f7e2059030d
 
 
 
@@ -38,7 +33,7 @@ export default function Home() {
           <Premium></Premium>
         </div>
         <div className="py-10">
-          {/* <Archive></Archive> */}
+         
         </div>
         
 
