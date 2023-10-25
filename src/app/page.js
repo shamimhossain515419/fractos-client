5e97bbd3f753b27ed847b7ba0920a318bf0eccba
Shamimusman515419 <shamimhossain01617@gmail.com>
2023-10-25 14:14:08 +0600

    banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,4 @@
 import Container from "@/Components/Container/Container";
-<<<<<<< HEAD
-import DashHome from "@/Components/Dashboard/DashHome";
-import Footer from "@/Components/Footer/Footer";
-import Pricing from "@/Components/Pricing/Pricing";
-import Banner from "./Home/Banner/Banner";
-import Premium from "@/Components/Premium/Premium";
-import CounterSection from "@/Components/CounterSection/CounterSection";
-=======
 
 import Footer from "@/Components/Footer/Footer";
 import Pricing from "@/Components/Pricing/Pricing";
@@ -21,25 +13,13 @@ import Navbar from "@/Components/Navbar";
 
 
 
->>>>>>> fe7f9ea30e5ac0312316dc250c7266e7e21e3104
 export default function Home() {
   return (
     <div>
       <Container>
-<<<<<<< HEAD
-        <Pricing></Pricing>
-        <Footer></Footer>
-        <Banner></Banner>
-        <Premium></Premium>
-        <CounterSection> </CounterSection>
-        <DashHome></DashHome>
-          
 
-    
-=======
-      
-          <Navbar></Navbar>
-       
+        <Navbar></Navbar>
+
         <Banner></Banner>
 
 
@@ -53,9 +33,9 @@ export default function Home() {
           <Premium></Premium>
         </div>
         <div className="py-10">
-         
+
         </div>
-        
+
 
 
         {/* Banner */}
@@ -63,7 +43,6 @@ export default function Home() {
         <ContactSection></ContactSection>
 
         <Footer></Footer>
->>>>>>> fe7f9ea30e5ac0312316dc250c7266e7e21e3104
       </Container>
 
     </div>
