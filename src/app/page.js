c00c31fd2d83087f320e33c3b8c87952d3d42694
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-10-26 13:50:51 +0600

    userProfile section try to complete

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,15 @@ import Container from "@/Components/Container/Container";
 
 import Footer from "@/Components/Footer/Footer";
 import Pricing from "@/Components/Pricing/Pricing";
+
+// import Banner from "./Home/Banner/Banner";
+
+
 import Premium from "@/Components/Premium/Premium";
 import CounterSection from "@/Components/CounterSection/CounterSection";
 import ContactSection from "@/Components/Contact";
-import Navbar from "@/Components/Navbar";
+import UserProfile from "@/Components/UserProfile/UserProfile";
 import Banner from "@/Home/Banner/Banner";
-import ExamModel from "@/Components/Shared/ExamModel";
-
-
 
 
 
@@ -17,16 +18,8 @@ export default function Home() {
   return (
     <div>
       <Container>
-      
-          <Navbar></Navbar>
-       
-
-        <Navbar></Navbar>
-        
         <Banner></Banner>
 
-        {/* exam question model */}
-        <ExamModel exam="Physics 2nd Paper - (Dhaka Board 2019)" >  </ExamModel>
 
         <div className=" my-3">
           <CounterSection> </CounterSection>
@@ -38,8 +31,9 @@ export default function Home() {
           <Premium></Premium>
         </div>
         <div className="py-10">
-
+          <UserProfile></UserProfile>
         </div>
+        
 
 
         {/* Banner */}
