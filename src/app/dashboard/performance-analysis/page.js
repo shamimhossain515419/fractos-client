482d60894da888a3d492d8336d482d5113e368ee
Abdur Rahman <abdurrahman27485@gmail.com>
2023-10-31 07:21:01 +0600

    performance analysis page design done

--- src/app/dashboard/performance-analysis/page.js
+++ src/app/dashboard/performance-analysis/page.js
@@ -1,7 +1,8 @@
 "use client"
 
-import React, { useEffect, useState } from 'react';
-import { Bar } from 'react-chartjs-2';
+import React from 'react';
+import { Bar, Doughnut } from 'react-chartjs-2';
+
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -10,7 +11,9 @@ import {
     Title,
     Tooltip,
     Legend,
+    ArcElement,
 } from 'chart.js';
+
 
 ChartJS.register(
     CategoryScale,
@@ -18,59 +21,134 @@ ChartJS.register(
     BarElement,
     Title,
     Tooltip,
-    Legend
-  );
-
-
+    Legend,
+    ArcElement
+);
 const page = () => {
-    const [barChartData, setBarChartData] = useState({
-        datasets: []
-    })
 
-    const [barChartOptions, setBarChartOptions] = useState({});
 
-    useEffect(() =>{
-        setBarChartData({
-            labels:['Skipped', 'Wrong', 'Correct'],
-            datasets:[
-                {
-                    labels: ['Skipped'],
-                    data: [10],
-                    backgroundColor: 'yellow'
-                },
-                {
-                    label: ['Wrong'],
-                    data: [20],
-                    backgroundColor: 'red'
-                },
-                {
-                    label: ['Correct'],
-                    data: [70],
-                    backgroundColor: 'green'
-                }
-            ]
-        })
-
-        setBarChartOptions({
-            plugins:{
-                legend:{
-                    position: 'bottom'
-                },
-                title:{
-                    display: true,
-                    text: 'Exam Result'
-                },
-                maintainAspectRatio: false,
-                responsive: true
+    // horizontal bar chart configurations
+    const horizontalBarChartData = {
+        labels: ['Math', 'Science', 'History', 'English'],
+        datasets: [
+            {
+                label: 'Wrong',
+                data: [8, 4, 5, 2], // Replace with your 'Wrong' data
+                backgroundColor: 'red',
+            },
+            {
+                label: 'Correct',
+                data: [5, 6, 7, 3], // Replace with your 'Correct' data
+                backgroundColor: 'green',
+            },
+            {
+                label: 'Skipped',
+                data: [3, 2, 1, 4], // Replace with your 'Skipped' data
+                backgroundColor: 'yellow',
+            },
+        ],
+    };
+    const horizontalBarChartOptions = {
+        indexAxis: 'y', // This sets the x-axis to be vertical
+        scales: {
+            x: {
+                stacked: true,
+            },
+            y: {
+                stacked: true,
+            },
+        },
+        plugins: {
+            legend: {
+                position: 'bottom'
             }
-        })
-    },[])
+        }
+    };
+
+    // doughnut chart configurations
+    const dougnutData = {
+        labels: ['Skipped', 'Wrong', 'Correct'],
+        datasets: [
+            {
+                data: [15, 30, 55],
+                backgroundColor: ['yellow', 'red', 'green'],
+            },
+        ],
+    };
+    const doughnutOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                position: 'bottom'
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    };
+
+
+    // bangla weakness chart
+    const BanWeaknessBarChartData = {
+        labels: ['সোনার তরী', 'আমার পথ', 'বিলাসি', 'অপরিচিতা', 'লালসালু', 'প্রতিদান'],
+        datasets: [
+            {
+                label: 'Wrong',
+                data: [8, 4, 5, 2], // Replace with your 'Wrong' data
+                backgroundColor: 'red',
+            },
+            {
+                label: 'Correct',
+                data: [5, 6, 7, 3], // Replace with your 'Correct' data
+                backgroundColor: 'green',
+            },
+            {
+                label: 'Skipped',
+                data: [3, 2, 1, 4], // Replace with your 'Skipped' data
+                backgroundColor: 'yellow',
+            },
+        ],
+    };
+
+    // for all vertical bar chart
+    const verticalBarChartOptions = {
+        scales: {
+            x: {
+                stacked: true,
+            },
+            y: {
+                stacked: true,
+            },
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    };
+
+
+
     return (
         <div>
+            <h2 className='text-2xl md:text-4xl text-[#0C0094] mb-10'>পারফর্মেন্স অ্যানালাইসিস</h2>
             <div className='grid grid-cols-1 md:grid-cols-12 gap-5'>
-                <div className='md:col-span-4 shadow-lg rounded-lg'></div>
-                <div className='md:col-span-8 shadow-lg rounded-lg'>
-                <Bar data={barChartData} options={barChartOptions}></Bar>
+                <div className='col-span-12 xl:col-span-4 p-2 lg:p-5 rounded-lg shadow-lg'>
+                    <h3 className='text-xl mb-5'>অ্যাকুরেসি</h3>
+                    <div className='h-[250px] w-[250px] mx-auto'>
+                        <Doughnut data={dougnutData} options={doughnutOptions} />
+                    </div>
+                </div>
+                <div className='col-span-12 xl:col-span-8 p-2 lg:p-5 rounded-lg shadow-lg'>
+                    <h3 className='text-xl mb-5'>বিষয়ভিত্তিক অ্যাকুরেসি</h3>
+                    <Bar data={horizontalBarChartData} options={horizontalBarChartOptions} />
+                </div>
+                <div className='col-span-12 xl:col-span-6 p-2 lg:p-5 rounded-lg shadow-lg'>
+                    <h3 className='text-xl mb-5'>দুর্বলতাঃ বাংলা</h3>
+                    <Bar data={BanWeaknessBarChartData} options={verticalBarChartOptions}></Bar>
                 </div>
             </div>
         </div>
