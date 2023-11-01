dd418759f2650ee23958d36c2ddc3ca600a16731
Ritupriya <ritupriyabgc@gmail.com>
2023-11-01 14:52:39 +0600

    QA dynamic route added

---
+++ src/app/dashboard/archive/du-c-unit/page.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import DUCUnit from '../../../../Components/ArchiveDUCUnit/DUCUnit';
+
+const DuCUnit  = () => {
+    return (
+        <div>
+            <DUCUnit></DUCUnit>
+        </div>
+    );
+};
+
+export default DuCUnit;
