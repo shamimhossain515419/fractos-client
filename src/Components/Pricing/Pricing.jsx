f30d4dd4ee4c88807d661fe9853109ac619bdfde
Abdur Rahman <abdurrahman27485@gmail.com>
2023-11-01 10:24:24 +0600

    basic work of pricing section ui improvement done

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -2,109 +2,68 @@ import React from 'react';
 import "./Pricing.css"
 import { FaCheckCircle } from 'react-icons/fa';
 import CommonTitle from '../CommonTitle/CommonTitle';
+import Image from 'next/image';
 
 
 const Pricing = () => {
     return (
-        <div className='py-10  rounded'>
-            <CommonTitle title={"Latest Price Of Our Classes"}>  </CommonTitle>
-            <h3 className='text-base md:text-lg  font-medium text-center '>Buy packages as per your needs</h3>
-
-            <div className="pricing-card-container grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3 px-3 mt-5  ">
-                <div className="card-item w-full py-10 bg-white rounded px-5 scale-90 hover:scale-100 duration-75">
-                    <h2 className='text-2xl font-bold text-center textColor'> 1 Video</h2>
-                    <h1 className='text-center font-bold text-5xl my-2 textColor'>$120</h1>
-                    <p className='text-center font-bold text-1xl mb-4 textColor'>Per video</p>
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Istagram stories</p>
+        <div className='py-[130px]'>
+            <CommonTitle title={"PLANS"}></CommonTitle>
+            {/* pricing section */}
+            <div className='flex gap-y-5 gap-x-20 justify-center items-start w-full'>
+                {/* basic price card */}
+                <div className='bg-white rounded-lg p-5'>
+                    <div className='flex items-center justify-between mb-5'>
+                        <h3 className='text-xl'>Basic</h3>
+                        <p>Free</p>
                     </div>
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>High Quality Video</p>
-                    </div>
-
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Up to 40 seconds</p>
-                    </div>
-
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Music Included</p>
-                    </div>
-
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Phone & Email Support</p>
-                    </div>
-
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Save up to 40% </p>
-                    </div>
-
-                    <button className='block  py-5 bg-blue-600 text-white rounded w-full font-bold hover:bg-blue-500 delay-100 mt-4'>Buy 1 video</button>
+                    <ul className='space-y-5'>
+                    <li className='flex items-center gap-2'>
+                            <Image src={'/warning.png'} height={20} width={20}></Image>
+                            <span>Mock Exam (8 Times)</span>
+                        </li>
+                    <li className='flex items-center gap-2'>
+                            <Image src={'/correct.png'} height={20} width={20}></Image>
+                            <span>Exam Review</span>
+                        </li>
+                    <li className='flex items-center gap-2'>
+                            <Image src={'/wrong.png'} height={20} width={20}></Image>
+                            <span>Live Exam</span>
+                        </li>
+                    <li className='flex items-center gap-2'>
+                            <Image src={'/wrong.png'} height={20} width={20}></Image>
+                            <span>Feedback & Support</span>
+                        </li>
+                    </ul>
+                    <button className='w-full rounded-lg bg-[#4A3AFF] text-white text-xl mt-5 py-2'>Get Started</button>
                 </div>
-
-                <div className="card-item w-full py-10 bg-white rounded px-5 scale-90 hover:scale-100 duration-75">
-                    <h2 className='text-2xl font-bold text-center textColor'> 6 Videos</h2>
-                    <h1 className='text-center font-bold text-5xl my-2 textColor'>$100</h1>
-                    <p className='text-center font-bold text-1xl mb-4 textColor'>Per video</p>
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Istagram stories</p>
+                {/* premium price card */}
+                <div className='bg-white rounded-lg p-5'>
+                    <div className='flex items-center justify-between mb-5'>
+                        <h3 className='text-xl'>Premium</h3>
+                        <p>$25</p>
                     </div>
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>High Quality Video</p>
-                    </div>
-
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Up to 3 minutes</p>
-                    </div>
-
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Music Included and slider</p>
-                    </div>
-
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Phone & Email Support</p>
-                    </div>
-
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Save up to 60% </p>
-                    </div>
-
-                    <button className='block  py-5 bg-blue-600 text-white rounded w-full font-bold hover:bg-blue-500 delay-100 mt-4'>Buy 6 videos</button>
+                    <ul className='space-y-5'>
+                        <li className='flex items-center gap-2'>
+                            <Image src={'/correct.png'} height={20} width={20}></Image>
+                            <span>Mock Exam</span>
+                        </li>
+                        <li className='flex items-center gap-2'>
+                            <Image src={'/correct.png'} height={20} width={20}></Image>
+                            <span>Exam Review</span>
+                        </li>
+                        <li className='flex items-center gap-2'>
+                            <Image src={'/correct.png'} height={20} width={20}></Image>
+                            <span>Live Exam</span>
+                        </li>
+                        <li className='flex items-center gap-2'>
+                            <Image src={'/correct.png'} height={20} width={20}></Image>
+                            <span>Feedback & Support</span>
+                        </li>
+                    </ul>
+                    <button className='w-full rounded-lg bg-[#4A3AFF] text-white text-xl mt-5 py-2'>Buy Now</button>
                 </div>
-
-
-
-                <div className="card-item w-full py-10 bg-white rounded px-5 scale-90 hover:scale-100 duration-75">
-                    <h2 className='text-2xl font-bold text-center textColor'> 12 Videos</h2>
-                    <h1 className='text-center font-bold text-5xl my-2 textColor'>$90</h1>
-                    <p className='text-center font-bold text-1xl mb-4 textColor'>Per video</p>
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Istagram stories</p>
-                    </div>
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>High Quality Video</p>
-                    </div>
-
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Up to 10 minutes</p>
-                    </div>
-
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Music Included</p>
-                    </div>
-
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Phone & Email Support</p>
-                    </div>
-
-                    <div className='flex flex-row space-x-2 mb-2 font-bold textColor'>
-                    <FaCheckCircle className='mt-1 text-green-500'></FaCheckCircle> <p>Save up to 60% </p>
-                    </div>
-
-                    <button className='block  py-5 bg-blue-600 text-white rounded w-full font-bold hover:bg-blue-500 delay-100 mt-4'>Buy 12 videos</button>
-                </div>
-
             </div>
-            
         </div>
     );
 };
