287357d217eb2b42605333949ffbf38f1f3e9843
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-11-01 17:49:26 +0600

    archive collage design complete

--- src/Components/Archive/ArchiveCollege.jsx
+++ src/Components/Archive/ArchiveCollege.jsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import { Parallax } from 'react-parallax';
+import './Archive.css';
 
 const ArchiveCollege = () => {
     const [colleges, setColleges] = useState([]);
@@ -18,8 +19,8 @@ const ArchiveCollege = () => {
     return (
         <>
             <div className=''>
-                <button className='bg-purple-950 m-3 p-2 font-bold rounded'>Academic</button>
-                <button className='bg-purple-950 m-3 p-2 font-bold rounded'>Admission</button>
+                <button className='bg-purple-950 m-3 p-2 font-bold rounded text-white'>Academic</button>
+                <button className='bg-purple-950 m-3 p-2 font-bold rounded text-white'>Admission</button>
             </div>
             <div className='grid md:grid-cols-3 lg:grid-cols-3 sm:grid-cols-2 gap-14'>
                 {
@@ -27,15 +28,16 @@ const ArchiveCollege = () => {
                         <div key={college._id}>
                             <div className='rounded'>
                                 {/* className="w-[50px] h-[50px]" */}
-                                <Parallax
-                                 bgImage={college.image} 
+                                {/* <Parallax
+                                 bgImage="https://i.ibb.co/y6LsMs5/versity.jpg" 
                                  bgImageAlt="the cat" 
                                  strength={200}
-                                 className="bg-cover bg-fixed h-36">
+                                 className="bg-cover bg-fixed h-36"> */}
+                                 <div className='backgroundImage h-32'>
                                     <div  className='p-8 rounded text-center'>
                                     <h1 className='text-2xl font-bold text-white'>{college.CollegeName}</h1>
-                                    </div>
-                                </Parallax>
+                                    </div></div>
+                                {/* </Parallax> */}
                                 <div className='bg-blue-50 rounded text-black font-bold p-3'>
                                     <p>{college.department}</p>
                                     <div className='flex justify-between mt-3'>
