84427b37de8e09f19e92740fa4d76efbae7cc657
Abdur Rahman <abdurrahman27485@gmail.com>
2023-11-02 16:01:25 +0600

    ui updated

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -10,9 +10,9 @@ const Pricing = () => {
         <div className='py-[130px]'>
             <CommonTitle title={"PLANS"}></CommonTitle>
             {/* pricing section */}
-            <div className='flex gap-y-5 gap-x-20 justify-center items-start w-full'>
+            <div className='flex gap-y-5 gap-x-20 justify-center items-start w-full mt-10'>
                 {/* basic price card */}
-                <div className='bg-white rounded-lg p-5 w-full md:min-w-[400px]'>
+                <div className='bg-slate-100 rounded-lg p-5 w-full md:min-w-[400px] shadow-lg border border-[#0c009455]'>
                     <div className='flex items-center justify-between mb-5'>
                         <h3 className='text-2xl md:text-4xl text-[#0C0094] font-bold'>Basic</h3>
                         <p className='font-semibold text-xl'>Free</p>
@@ -43,10 +43,10 @@ const Pricing = () => {
                             <span className='text-xl font-semibold'>Performance Analysis</span>
                         </li>
                     </ul>
-                    <button className='w-full rounded-lg outline-slate-500 outline-4 hover:bg-[#4A3AFF] hover:text-white text-xl mt-10 transition-all font-bold py-5'>Get Started</button>
+                    <button className='w-full rounded-lg border-2 border-blue-600  hover:bg-[#4A3AFF] hover:text-white text-xl mt-10 transition-all font-bold py-5'>Get Started</button>
                 </div>
                 {/* premium price card */}
-                <div className='bg-white rounded-lg p-5 w-full md:min-w-[400px]'>
+                <div className='bg-slate-100 rounded-lg p-5 w-full md:min-w-[400px] shadow-lg border border-[#0c009455]'>
                     <div className='flex items-center justify-between mb-5'>
                         <h3 className='text-2xl md:text-4xl text-[#0C0094] font-bold'>Premium</h3>
                         <p className='font-semibold text-xl'>$25</p>
