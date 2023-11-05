cda2d0f0f16f3158804f903c3f4c69a9b3801390
Abdur Rahman <abdurrahman27485@gmail.com>
2023-11-05 18:56:29 +0600

    teacher pages added

---
+++ src/app/teacher/page.js
@@ -0,0 +1,12 @@
+import Navbar from '@/Components/Navbar';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1>This is teacher page</h1>
+        </div>
+    );
+};
+
+export default page;
