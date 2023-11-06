99bf73ac24a15e0861f63ef3f8ec1b79f76f59cd
Ritupriya <ritupriyabgc@gmail.com>
2023-11-06 16:39:40 +0600

    update teacher profile

--- src/Components/TeacherProfile/TeacherProfile.jsx
+++ src/Components/TeacherProfile/TeacherProfile.jsx
@@ -18,26 +18,26 @@ const TeacherProfile = () => {
     
     return (
         <div>
-           <div className="grid grid-cols-2 gap-3 mx-8 my-12">
-            <div className="col lg:mt-28 text-left">
+           <div className="grid grid-cols-2 gap-0 lg:gap-2 mx-8 my-12">
+            <div className="col mt-16 lg:mt-28 text-left">
                 <p className='font-bold text-xl lg:text-5xl md:text-2xl mb-4 md:mb-6 '>{data.name}</p>
                 <p className=' lg:text-xl ml-2'>{data.qualification1}</p>
                 <p className=' lg:text-xl ml-2'>{data.qualification2}</p>
                 <p className=' lg:text-xl ml-2'>{data.achievement}</p>
-                <div className='flex'>
+                <div className='lg:text-xl ml-2 flex'>
                       <div className="">
                    
                     <p>{data.batch} Batches</p>
                       </div>
-                      <div className=" mr-4">
+                      <div className=" ml-4">
                      
                     <p>{data.enrolled_student} Students</p>
                       </div>
                 </div>
                 
             </div>
-            <div className="col">
-            <Image  width={100} height={100} src={img} alt="download" className='w-9/12 '>
+            <div className="col lg:mr-8">
+            <Image  src={img} alt="download" className='w-full h-96'>
 
             </Image>
             </div>
