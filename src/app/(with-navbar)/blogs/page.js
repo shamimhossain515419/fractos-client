2b72d1fd28f872372f1c51baaf82fc56c723e8af
Shamimusman515419 <shamimhossain01617@gmail.com>
2023-11-07 22:18:31 +0600

    abdur

--- src/app/(with-navbar)/blogs/page.js
+++ src/app/(with-navbar)/blogs/page.js
@@ -6,8 +6,8 @@ import React from 'react';
 const page = () => {
     const headingTextClasses = 'text-2xl lg:text-3xl text-white font-bold  mb-5'
     return (
-        <div className='w-full p-2 lg:p-0 lg:w-3/4 mx-auto mb-[130px]'>
-            <h1 className={`${headingTextClasses} pt-8`}>Featured</h1>
+        <div className= '   w-full p-2 lg:p-0 lg:w-3/4 mx-auto mb-[130px]'>
+            <h1 className={`${headingTextClasses} pt-16`}>Featured</h1>
             <FeaturedBlogCard></FeaturedBlogCard>
             <h1 className={`${headingTextClasses}`}>All Blogs</h1>
             {/* ALL BLOGS */}
