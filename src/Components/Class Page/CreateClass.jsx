3de80330bc0430d561ac9fabc85124c4af2ca4d8
Shamiul haque <shamiulhaque1218@gmail.com>
2023-11-07 23:02:05 +0600

    input field design

--- src/Components/Class Page/CreateClass.jsx
+++ src/Components/Class Page/CreateClass.jsx
@@ -21,17 +21,17 @@ const CreateClass = () => {
     return (
           <> 
           <div className="pt-20">
-          <p className="gFont2 text-5xl  py-10  px-10">Add a new class</p> 
+          <p className="gFont2 lg:text-5xl text-3xl  py-10  px-10">Add a new class</p> 
           </div> 
             <div className="px-20 pb-20">
               
-              <Link href="/" className='ml-10' to="/" > <button className="mb-5 px-2 py-2 text-center font-bold text-white mt-5 bg-green-800 rounded-md hover:bg-green-900" >
+              <Link href="/" className='lg:ml-10' to="/" > <button className="mb-5 px-2 py-2 text-center font-bold text-white mt-5 bg-green-800 rounded-md hover:bg-green-900" >
                Back to Dashboard
              </button> </Link>
 
       <form  onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 lg:grid-cols-2">
 
-        <div className="mb-4 mx-10">
+        <div className="mb-4 lg:mx-10">
           <label
             htmlFor="pic"
             className="block mb-2 text-sm font-bold text-gray-200"
@@ -42,11 +42,11 @@ const CreateClass = () => {
             type="text"
             name="pic"
             {...register("pic", { required: true })}
-            className="w-full px-3 py-1 placeholder-white border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 border-white text-black"
+            className="lg:px-20 py-3 placeholder-white border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 border-white text-black"
           />
         </div>
 
-        <div className="mb-4 mx-10">
+        <div className="mb-4 lg:mx-10">
           <label
             htmlFor="des"
             className="block mb-2 text-sm font-bold text-gray-200"
@@ -57,11 +57,11 @@ const CreateClass = () => {
             type="text"
             name="des"
             {...register("des", { required: true })}
-            className="w-full px-3 py-1 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 border-white text-black"
+            className="lg:px-20 py-3 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 border-white text-black"
           />
         </div>
 
-        <div className="mb-4 mx-10 mt-5">
+        <div className="mb-4 lg:mx-10 mt-5">
           <label
             htmlFor="price"
             className="block mb-2 text-sm font-bold text-gray-200"
@@ -72,13 +72,13 @@ const CreateClass = () => {
             type="text"
             name="price"
             {...register("price", { required: true })}
-            className="w-full px-3 py-1 placeholder-white border rounded-md focus:ring-2 focus:ring-blue-500 border-white text-black"
+            className="lg:px-20 py-3 placeholder-white border rounded-md focus:ring-2 focus:ring-blue-500 border-white text-black"
           />
         </div>
 
         <button
           type="submit"
-          className="mx-10 my-8 px-4 py-2 text-center font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600"
+          className="lg:ml-36 ml-9 mt-10 lg:mr-72 mr-20 my-7 py-4 text-center font-bold text-white bg-green-700 rounded-full hover:bg-green-800"
         >
           Add Class
         </button>
