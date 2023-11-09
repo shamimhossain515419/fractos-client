89eac9cd37b730cd6eb6cf985bfd906683db5ecb
Abdur Rahman <abdurrahman27485@gmail.com>
2023-11-09 11:48:18 +0600

    activeLink className

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -11,14 +11,15 @@ import CommonImage from "../CommonImage/CommonImage";
 import UserModal from "../UserModal/UserModal";
 import Image from "next/image";
 import logo from '../../../public/logo.png'
+import { usePathname } from "next/navigation";
 const Navbar = () => {
      const { userinfo, user, setIsAdmin, isAdmin, setIsUserModalOpen, isUserModalOpen } = useContext(GlobalContext);
      const [Open, setOpen] = useState(true);
-
+     const pathName = usePathname()
      return (
           <div className=" mb-12">
                <div>
-                    <nav className={`px-2 py-1 z-50   primaryBg shadow-lg fixed top-0 left-0 w-full transition-transform`}>
+                    <nav className={`px-2 py-3 z-50 primaryBg shadow-lg fixed top-0 left-0 w-full transition-transform`}>
                          <Container>
                               <div className=' '>
                                    <div className='flex justify-between items-center'>
@@ -41,11 +42,11 @@ const Navbar = () => {
                                         </div>
 
                                         <div className=' hidden md:flex items-center gap-3  space-x-5'>
-                                             <Link className="p-2 rounded-lg hover:bg-[#27895C] font-medium transition-all" href={'/'}>Home</Link>
-                                             <Link className="p-2 rounded-lg hover:bg-[#27895C] font-medium transition-all" href={'/teacher'}>Teacher</Link>
-                                             <Link className="p-2 rounded-lg hover:bg-[#27895C] font-medium transition-all" href={'/courses'}> All Courses</Link>
-                                             <Link className="p-2 rounded-lg hover:bg-[#27895C] font-medium transition-all" href={'/blogs'}>Blogs</Link>
-                                             <Link className="p-2 rounded-lg hover:bg-[#27895C] font-medium transition-all" href={'/contact'}>Contact Us</Link>
+                                             <Link className={`px-2 rounded-lg ${pathName == "/" ? "activeLink" : ""} hover:bg-[#0EE6B8] hover:text-black font-medium transition-all`} href={'/'}>Home</Link>
+                                             <Link className={`px-2 rounded-lg ${pathName == "/teacher" ? "activeLink" : ""} hover:bg-[#0EE6B8] hover:text-black font-medium transition-all`} href={'/teacher'}>Teacher</Link>
+                                             <Link className={`px-2 rounded-lg ${pathName == "/courses" ? "activeLink" : ""} hover:bg-[#0EE6B8] hover:text-black font-medium transition-all`} href={'/courses'}> All Courses</Link>
+                                             <Link className={`px-2 rounded-lg ${pathName == "/blogs" ? "activeLink" : ""} hover:bg-[#0EE6B8] hover:text-black font-medium transition-all`} href={'/blogs'}>Blogs</Link>
+                                             <Link className={`px-2 rounded-lg ${pathName == "/contact" ? "activeLink" : ""} hover:bg-[#0EE6B8] hover:text-black font-medium transition-all`} href={'/contact'}>Contact Us</Link>
 
                                         </div>
 
@@ -56,7 +57,7 @@ const Navbar = () => {
                                                   user ?
                                                        <div className='relative cursor-pointer hidden md:block '>
                                                             <div onClick={() => setIsUserModalOpen(!isUserModalOpen)} className='flex gap-x-2 items-center py-1  px-2 rounded-lg hover:bg-[#27895C] font-medium transition-all'>
-                                                            <Image className=" rounded-full w-[46px] h-[46px]" src={user?.photoURL ? user?.photoURL  : "https://i.ibb.co/XpNhB9s/beautiful-woman-avatar-character-icon-free-vector.jpg"} alt="" height={46} width={46} />
+                                                                 <Image className=" rounded-full w-[46px] h-[46px]" src={user?.photoURL ? user?.photoURL : "https://i.ibb.co/XpNhB9s/beautiful-woman-avatar-character-icon-free-vector.jpg"} alt="" height={46} width={46} />
                                                                  <h1 className='relative  text-base'>{user?.displayName}</h1>
                                                             </div>
                                                             {
@@ -70,8 +71,8 @@ const Navbar = () => {
                                                        </div>
                                                        :
                                                        <div className="flex gap-2 items-center">
-                                                            <Link href={'/login'} className="px-5 py-2 border-2 hover:border-white transition-all border-[#27895C] text-white bg-[#27895C] hover:bg-[#black font-mediumFe1] rounded-full font-mediumer:shadow-lg">Login</Link>
-                                                            <Link href={'/register'} className="px-3 py-2 bg-transparent text-white border-2 border-slate-600 hover:bg-slate-600 rounded-full hover:shadow-lg transition-all">Register</Link>
+                                                            <Link href={'/login'} className="px-5 border-2 hover:border-white transition-all border-[#27895C] text-white bg-[#27895C] hover:bg-[#black font-mediumFe1] rounded-full font-mediumer:shadow-lg">Login</Link>
+                                                            <Link href={'/register'} className="px-3 bg-transparent text-white border-2 border-slate-600 hover:bg-slate-600 rounded-full hover:shadow-lg transition-all">Register</Link>
                                                        </div>
                                              }
                                         </div>
