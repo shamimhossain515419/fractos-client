8346c82471deffbfdac6e3f637ccc08f796669c9
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-11-10 14:55:11 +0600

    table space setup

--- src/Components/ArchiveDUCUnit/ArchiveCollege.jsx
+++ src/Components/ArchiveDUCUnit/ArchiveCollege.jsx
@@ -27,7 +27,6 @@ const ArchiveCollege = async () => {
     console.log(search);
     return (
         <>
-
             <div className='  md:flex   justify-start items-center'>
                 <div className=' text-center mx-auto'>
                     <button onClick={() => setOpen(true)} className={`${Open ? " buttonColor text-black" : "border border-[#0EE6B8]  text-white "}   m-3 p-2 font-bold rounded `}> Admission</button>
@@ -36,25 +35,15 @@ const ArchiveCollege = async () => {
                 <div className=' my-16'>
                     <div id="searchBar-part">
                         <Container>
-
                             <div className="box   px-2 py-3 relative">
-
                                 <input
                                     className="w-full  primaryBg rounded-3xl px-7    py-5  bg-transparent  border-b focus:outline-none"
                                     type="text"
                                     id="search"
                                       onChange={handleChange}
                                     placeholder="Search "
-
-
                                 />
-
-
-
                             </div>
-
-
-
                         </Container>
                     </div>
 
@@ -65,11 +54,11 @@ const ArchiveCollege = async () => {
                     colleges?.map(college =>
                         <Link href={`/dashboard/question-bank/${college.path}`} key={college._id}>
                             <div className='rounded'>
-                                <div className='backgroundImage h-32'>
+                                <div className='backgroundImage'>
                                     <div className='p-8 rounded text-center'>
                                         <h1 className='text-2xl font-bold text-white'>{college.CollegeName}</h1>
-                                    </div></div>
-                                {/* </Parallax> */}
+                                    </div>
+                                </div>
                                 <div className=' secondBg rounded text-white font-bold p-3'>
                                     <p className=' capitalize secondColor'>{college.department}</p>
                                     <div className='flex justify-between mt-3'>
@@ -81,7 +70,6 @@ const ArchiveCollege = async () => {
                         </Link>)
                 }
             </div>
-
         </>
     );
 };
