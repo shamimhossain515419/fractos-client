889c1b8153b0a89da4062e4b27398a995d765c5a
ratrikhanam000 <ratrikhanam000@gmail.com>
2023-11-13 23:25:43 +0600

    enroll page design create

---
+++ src/Components/Enroll/Enroll.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Container from '../Container/Container';
+// import Container from '../Container/Container';
+
+const Enroll = () => {
+    return (
+        <div>
+            <Container>
+                <div className='py-8 secondBg px-3'>
+
+                    <div className="md:flex boxShodowClass border border-[#0EE6B8]    mx-4 my-6 rounded-lg ">
+                        <div className='md:w-1/2  p-4 '>
+                            {/* <Image width={100} height={100} src={details.image} alt="download" border="0" className='w-full h-[400px] md:mt-4 p-4 rounded-lg   '></Image> */}
+                            <img src="https://i.ibb.co/GVs8N9g/user1.jpg" className='w-96 h-60' alt="" />
+                        </div>
+                        <div className='p-4 font-bold'>
+                            <p>Subject : </p>
+                            <p>Description : </p>
+                            <p>Price : </p>
+                            <p>Limit : </p>
+                            <p>Time : </p>
+                        </div>
+                    </div>
+                    <div className='text-center'>
+                        <div>
+                            <p className='py-2'>Name : </p>
+                            <input type="text" placeholder='Enter Your Name'
+                                className='border-[#0EE6B8] border-2 py-2 w-2/4 px-3 bg-[#192035]' />
+                        </div>
+                        <div>
+                            <p className='py-2'>Email : </p>
+                            <input type="email" placeholder='Enter Your Email' className='border-[#0EE6B8] border-2 px-3 py-2 w-2/4 bg-[#192035]' />
+                        </div>
+                        <div>
+                            <p className='py-2'>Address : </p>
+                            <input type="text" placeholder='Enter Your Address' className='border-[#0EE6B8] px-3 border-2 py-2 w-2/4 bg-[#192035]' />
+                        </div>
+                    </div>
+                    <div className='flex justify-end mt-4'>
+                        <button className=' font-bold text-xl border border-[#0EE6B8]  rounded-lg  p-3 hover:bg-[#0EE6B8] hover:text-black  mt-4 md:mt-0'>Enroll Now</button>
+                    </div>
+
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Enroll;
--- src/Components/course/AllCourse.jsx
+++ src/Components/course/AllCourse.jsx
@@ -5,26 +5,24 @@ import Container from '../Container/Container';
 import { GetCourses } from '@/services/courses';
 // import BatchDetails from './BatchDetails';
 
-const AllCourse =async () => {
-  const dataad = await GetCourses();
-       console.log(dataad);
+const AllCourse = async () => {
+    const dataad = await GetCourses();
+    console.log(dataad);
 
     return (
-        <Container>
+            <Container>
 
-            <div className='text-center py-9'>
-                <h1 className='font-bold text-2xl md:text-3xl text-[#3eeac5]'>All Online Batches</h1>
-            <p className='my-4 text-[#9cf9e5]'>Join With Our Batch And Prepare You From Any Part of Our Country</p>
-            </div>
-            
-            <div className='mx-4 my-4 flex justify-center items-center   min-h-[60vh]'>
-                
-                <HscBatch></HscBatch>
-            </div>
-          
+                <div className='text-center py-9'>
+                    <h1 className='font-bold text-2xl md:text-3xl text-[#3eeac5]'>All Online Batches</h1>
+                    <p className='my-4 text-[#9cf9e5]'>Join With Our Batch And Prepare You From Any Part of Our Country</p>
+                </div>
 
-            
-        </Container>
+                <div className='mx-4 my-4 flex justify-center items-center   min-h-[60vh]'>
+
+                    <HscBatch></HscBatch>
+                </div>
+            </Container>
+
     );
 };
 
